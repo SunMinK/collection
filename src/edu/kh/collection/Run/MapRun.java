978f179e114c8f4b1a9848aee1b0b49ea1c589ac
SunMinK <sunnysunm98@gmail.com>
2022-12-29 14:59:02 +0900

    collection

---
+++ src/edu/kh/collection/Run/MapRun.java
@@ -0,0 +1,15 @@
+package edu.kh.collection.Run;
+
+import edu.kh.collection.model.servcie.MapService;
+
+public class MapRun {
+
+	public static void main(String[] args) {
+		
+		MapService service = new MapService();
+		service.ex3();
+		//service
+
+	}
+
+}
---
+++ src/edu/kh/collection/Run/SetRun.java
@@ -0,0 +1,15 @@
+package edu.kh.collection.Run;
+
+import edu.kh.collection.model.servcie.SetService;
+
+public class SetRun {
+
+	public static void main(String[] args) {
+		//SetService service = new SetService();
+		//service.lotto();
+		
+		
+
+	}
+
+}
